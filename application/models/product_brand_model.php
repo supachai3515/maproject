4e1910fc79d2cdc3183a6f063266fa540aa13a5b
wisadev <supachai.wi@gmail.com>
2017-06-27 09:50:16 +0700

    sql injetion

--- application/models/product_brand_model.php
+++ application/models/product_brand_model.php
@@ -9,10 +9,15 @@ class Product_brand_model extends CI_Model {
 
 	function get_product_brand_count($searchText = '')
 	{
-
+		// $this->db->escape() ใส่ '' ให้
+		// $this->db->escape_str()  ไม่ใส่ '' ให้
+		// $this->db->escape_like_str($searchText) like
+		$searchText = $this->db->escape_like_str($searchText);
 		$sql =" SELECT COUNT(m.product_brand_id) as connt_id FROM  product_brand m WHERE 1=1 ";
 		if(!empty($searchText)) {
-				$sql = $sql." AND (m.product_brand_id  LIKE '%".$searchText."%' OR  m.name  LIKE '%".$searchText."%' OR  m.description  LIKE '%".$searchText."%')";
+				$sql = $sql." AND (m.product_brand_id  LIKE '%".$searchText."%'
+														OR  m.name  LIKE '%".$searchText."%'
+														OR  m.description  LIKE '%".$searchText."%')";
 		}
 		$query = $this->db->query($sql);
 		$row = $query->row_array();
@@ -22,11 +27,17 @@ class Product_brand_model extends CI_Model {
 
   function get_product_brand($searchText = '', $page, $segment)
   {
+		$searchText = $this->db->escape_like_str($searchText);
+		$page = $this->db->escape_str($page);
+		$segment = $this->db->escape_str($segment);
+
 		$sql ="SELECT m.* , u1.name create_by_name , u2.name  modified_by_name FROM  product_brand m
 						LEFT JOIN tbl_users u1 ON u1.userId = m.create_by
 						LEFT JOIN tbl_users u2 ON u2.userId = m.modified_by WHERE 1=1 ";
 		if(!empty($searchText)) {
-				$sql = $sql." AND (m.product_brand_id  LIKE '%".$searchText."%' OR  m.name  LIKE '%".$searchText."%' OR  m.description  LIKE '%".$searchText."%')";
+				$sql = $sql." AND (m.product_brand_id  LIKE '%".$searchText."%'
+													OR  m.name  LIKE '%".$searchText."%'
+													OR  m.description  LIKE '%".$searchText."%')";
 		}
 			$sql = $sql." LIMIT ".$page.",".$segment." ";
       $query = $this->db->query($sql);
@@ -46,10 +57,13 @@ class Product_brand_model extends CI_Model {
 
 	public function get_product_brand_id($id)
 	{
+
+		$id = $this->db->escape($id);
+
 		$sql ="SELECT m.* , u1.name create_by_name , u2.name  modified_by_name FROM  product_brand m
 						LEFT JOIN tbl_users u1 ON u1.userId = m.create_by
 						LEFT JOIN tbl_users u2 ON u2.userId = m.modified_by
-						 WHERE m.product_brand_id = '".$id."'";
+						WHERE m.product_brand_id = ".$id;
 		$query = $this->db->query($sql);
 		$row = $query->row_array();
 		return $row;
