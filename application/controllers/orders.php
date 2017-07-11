34aca2fa9a2957c5360825ec24e57986482e39aa
wisadev <supachai.wi@gmail.com>
2017-07-11 23:15:40 +0700

    orders

---
+++ application/controllers/orders.php
@@ -0,0 +1,246 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/BaseController.php';
+
+class Orders extends BaseController {
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('initdata_model');
+		$this->load->model('orders_model');
+    $this->isLoggedIn();
+  }
+
+  function index($page=0)
+  {
+    $data['global'] = $this->global;
+    $data['menu_id'] ='15';
+		$data['menu_list'] = $this->initdata_model->get_menu($data['global']['menu_group_id']);
+    $data['access_menu'] = $this->isAccessMenu($data['menu_list'],$data['menu_id']);
+    if($data['access_menu']['is_access']&&$data['access_menu']['is_view'])
+    {
+      if($page != 0)
+      {
+        if (!ctype_digit($page)) {
+          redirect('error');
+        }
+      }
+
+      $searchText = $this->input->post('searchText');
+      $data['searchText'] = $searchText;
+      $count = $this->orders_model->get_orders_count($searchText);
+      $data['links_pagination'] = $this->pagination_compress( "orders/index", $count, $this->config->item('pre_page') );
+  	  $data['orders_list'] = $this->orders_model->get_orders($searchText, $page, $this->config->item('pre_page'));
+
+
+      $data['content'] = 'orders/orders_view';
+      //if script file
+      //$data['script_file'] = 'js/orders_js';
+  		$data['header'] = array('title' => 'Product Brand | '.$this->config->item('sitename'),
+              								'description' =>  'Product Brand | '.$this->config->item('tagline'),
+              								'author' => $this->config->item('author'),
+              								'keyword' => 'Product Brand');
+  		$this->load->view('template/layout_main', $data);
+    }
+    else {
+      // access denied
+       $this->loadThis();
+    }
+  }
+
+  function add()
+  {
+    $data['global'] = $this->global;
+    $data['menu_id'] ='15';
+		$data['menu_list'] = $this->initdata_model->get_menu($data['global']['menu_group_id']);
+    $data['access_menu'] = $this->isAccessMenu($data['menu_list'],$data['menu_id']);
+    if($data['access_menu']['is_access']&&$data['access_menu']['is_add'])
+    {
+        $data['content'] = 'orders/orders_add_view';
+        //if script file
+        //$data['script_file'] = 'js/orders_js';
+  		  $data['header'] = array('title' => 'Add Product Brand | '.$this->config->item('sitename'),
+              								'description' =>  'Add Product Brand | '.$this->config->item('tagline'),
+              								'author' => $this->config->item('author'),
+              								'keyword' => 'Product Brand');
+  		  $this->load->view('template/layout_main', $data);
+    }
+    else {
+      // access denied
+       $this->loadThis();
+    }
+  }
+
+  function view($id=NULL)
+  {
+    $data['global'] = $this->global;
+    $data['menu_id'] ='15';
+    $data['menu_list'] = $this->initdata_model->get_menu($data['global']['menu_group_id']);
+    $data['access_menu'] = $this->isAccessMenu($data['menu_list'],$data['menu_id']);
+    if($data['access_menu']['is_access']&&$data['access_menu']['is_view'])
+    {
+      if($id == null)
+      {
+          redirect('orders');
+      }
+      else {
+        if (!ctype_digit($id)) {
+          redirect('error');
+        }
+      }
+
+        $data['orders_data'] = $this->orders_model->get_orders_id($id);
+
+        if(count($data['orders_data'])==0){
+            redirect('error');
+        }
+
+        $data['content'] = 'orders/orders_info_view';
+        //if script file
+        //$data['script_file'] = 'js/orders_js';
+        $data['header'] = array('title' => 'View Product Brand | '.$this->config->item('sitename'),
+                              'description' =>  'View Product Brand | '.$this->config->item('tagline'),
+                              'author' => $this->config->item('author'),
+                              'keyword' => 'Product Brand');
+        $this->load->view('template/layout_main', $data);
+    }
+    else {
+      // access denied
+       $this->loadThis();
+    }
+  }
+
+
+  function add_save()
+  {
+    $data['global'] = $this->global;
+    $data['menu_id'] ='15';
+		$data['menu_list'] = $this->initdata_model->get_menu($data['global']['menu_group_id']);
+    $data['access_menu'] = $this->isAccessMenu($data['menu_list'],$data['menu_id']);
+    if($data['access_menu']['is_access']&&$data['access_menu']['is_add'])
+    {
+          $this->load->library('form_validation');
+          $this->form_validation->set_rules('name','Name','trim|required|max_length[128]|xss_clean');
+          $this->form_validation->set_rules('description','description','trim|xss_clean|max_length[128]');
+          $this->form_validation->set_rules('is_active','ใช้งาน','');
+
+          if($this->form_validation->run() == FALSE)
+          {
+              $this->add();
+          }
+          else
+          {
+              $name = $this->input->post('name');
+              $description = $this->input->post('description');
+              $is_active = $this->input->post('is_active');
+
+              $orders_info = array('name'=>$name, 'description'=>$description, 'is_active'=>$is_active,
+                                      'create_by'=>$this->vendorId, 'create_date'=>date('Y-m-d H:i:s'),
+                                      'modified_by'=>$this->vendorId, 'modified_date'=>date('Y-m-d H:i:s'));
+
+              $result = $this->orders_model->save_orders($orders_info);
+
+              if($result > 0)
+              {
+                  $this->session->set_flashdata('success', 'Add Product Brand created successfully');
+              }
+              else
+              {
+                  $this->session->set_flashdata('error', 'User creation failed');
+              }
+              redirect('orders/add');
+          }
+      }
+      else {
+           $this->loadThis();
+      }
+  }
+
+  function edit($id=NULL)
+  {
+    $data['global'] = $this->global;
+    $data['menu_id'] ='15';
+		$data['menu_list'] = $this->initdata_model->get_menu($data['global']['menu_group_id']);
+    $data['access_menu'] = $this->isAccessMenu($data['menu_list'],$data['menu_id']);
+    if($data['access_menu']['is_access']&&$data['access_menu']['is_edit'])
+    {
+        if($id == null)
+        {
+            redirect('orders');
+        }
+        else {
+          if (!ctype_digit($id)) {
+            redirect('error');
+          }
+        }
+
+
+        $data['orders_data'] = $this->orders_model->get_orders_id($id);
+        if(count($data['orders_data'])==0){
+            redirect('error');
+        }
+
+        $data['content'] = 'orders/orders_edit_view';
+        //if script file
+        //$data['script_file'] = 'js/orders_js';
+  		  $data['header'] = array('title' => 'Add Product Brand | '.$this->config->item('sitename'),
+              								'description' =>  'Add Product Brand | '.$this->config->item('tagline'),
+              								'author' => $this->config->item('author'),
+              								'keyword' => 'Product Brand');
+  		  $this->load->view('template/layout_main', $data);
+    }
+    else {
+      // access denied
+       $this->loadThis();
+    }
+  }
+
+
+  function edit_save()
+  {
+    $data['global'] = $this->global;
+    $data['menu_id'] ='15';
+		$data['menu_list'] = $this->initdata_model->get_menu($data['global']['menu_group_id']);
+    $data['access_menu'] = $this->isAccessMenu($data['menu_list'],$data['menu_id']);
+    if($data['access_menu']['is_access']&&$data['access_menu']['is_add'])
+    {
+          $this->load->library('form_validation');
+          $this->form_validation->set_rules('name','Name','trim|required|max_length[128]|xss_clean');
+          $this->form_validation->set_rules('description','description','trim|xss_clean|max_length[128]');
+          $this->form_validation->set_rules('is_active','ใช้งาน','');
+
+          if($this->form_validation->run() == FALSE)
+          {
+              $this->add();
+          }
+          else
+          {
+              $name = $this->input->post('name');
+              $description = $this->input->post('description');
+              $is_active = $this->input->post('is_active');
+              $orders_id = $this->input->post('orders_id');
+
+              $orders_info = array('name'=>$name, 'description'=>$description, 'is_active'=>$is_active,
+                                      'orders_id'=>$orders_id,
+                                      'modified_by'=>$this->vendorId,
+                                      'modified_date'=>date('Y-m-d H:i:s'));
+
+              $result = $this->orders_model->update_orders($orders_info,$orders_id);
+
+              if($result > 0)
+              {
+                  $this->session->set_flashdata('success', 'Edit Product Brand Update successfully');
+              }
+              else
+              {
+                  $this->session->set_flashdata('error', 'User creation failed');
+              }
+              redirect('orders/edit/'.$orders_id);
+          }
+      }
+      else {
+           $this->loadThis();
+      }
+  }
+}
---
+++ application/models/orders_model.php
@@ -0,0 +1,124 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class orders_model extends CI_Model {
+	public function __construct(){
+		parent::__construct();
+	}
+
+	function get_orders_count($searchText = '')
+	{
+		// $this->db->escape() ใส่ '' ให้
+		// $this->db->escape_str()  ไม่ใส่ '' ให้
+		// $this->db->escape_like_str($searchText) like
+		$searchText = $this->db->escape_like_str($searchText);
+		$sql =" SELECT COUNT(r.order_id) as connt_id FROM  orders  r
+    LEFT JOIN tbl_users u1 ON u1.userId = r.create_by
+    LEFT JOIN tbl_users u2 ON u2.userId = r.modified_by
+    LEFT JOIN order_status  os ON os.order_status_id = r.order_status_id WHERE 1=1 ";
+		if(!empty($searchText)) {
+				$sql = $sql." AND ( r.order_id  LIKE '%".$searchText."%'
+														OR  r.company  LIKE '%".$searchText."%'
+                            OR  r.ref_id  LIKE '%".$searchText."%'
+                            OR  r.tel  LIKE '%".$searchText."%'
+														OR  r.email  LIKE '%".$searchText."%')";
+		}
+		$query = $this->db->query($sql);
+		$row = $query->row_array();
+		return  $row['connt_id'];
+
+	}
+
+
+  public function get_order($order_id)
+	{
+		$order_id = $this->db->escape_str($order_id);
+		$sql =" SELECT r.* , u1.name create_by_name , u2.name  modified_by_name ,os.`name` status_name
+						FROM  orders  r
+						LEFT JOIN tbl_users u1 ON u1.userId = r.create_by
+						LEFT JOIN tbl_users u2 ON u2.userId = r.modified_by
+						LEFT JOIN order_status  os ON os.order_status_id = r.order_status_id
+						WHERE r.order_id  =  '".$order_id."'";
+		$query = $this->db->query($sql);
+		$row = $query->row_array();
+		return $row;
+	}
+
+	public function get_order_detail($order_id)
+	{
+		$order_id = $this->db->escape_str($order_id);
+		$sql =" SELECT pw.part_number,pw.`name` product_name, pw.description product_description, od.* FROM order_detail od
+				LEFT JOIN product_owner pw ON pw.product_owner_id = od.product_owner_id
+				LEFT JOIN product_vendor pv ON pv.product_vendor_id = od.product_vendor_id
+				WHERE od.order_id  =  '".$order_id."'";
+		$query = $this->db->query($sql);
+		$result = $query->result_array();
+		return $result;
+	}
+
+
+  function get_orders($searchText = '', $page, $segment)
+  {
+		$searchText = $this->db->escape_like_str($searchText);
+		$page = $this->db->escape_str($page);
+		$segment = $this->db->escape_str($segment);
+
+		$sql ="SELECT m.* , u1.name create_by_name , u2.name  modified_by_name FROM  orders m
+						LEFT JOIN tbl_users u1 ON u1.userId = m.create_by
+						LEFT JOIN tbl_users u2 ON u2.userId = m.modified_by WHERE 1=1 ";
+		if(!empty($searchText)) {
+				$sql = $sql." AND (m.orders_id  LIKE '%".$searchText."%'
+													OR  m.name  LIKE '%".$searchText."%'
+													OR  m.description  LIKE '%".$searchText."%')";
+		}
+			$sql = $sql." LIMIT ".$page.",".$segment." ";
+      $query = $this->db->query($sql);
+      $result = $query->result();
+      return $result;
+  }
+
+	function get_orders_all()
+  {
+		$sql ="SELECT m.* , u1.name create_by_name , u2.name  modified_by_name FROM  orders m
+						LEFT JOIN tbl_users u1 ON u1.userId = m.create_by
+						LEFT JOIN tbl_users u2 ON u2.userId = m.modified_by WHERE 1=1 ";
+      $query = $this->db->query($sql);
+      $result = $query->result();
+      return $result;
+  }
+
+	public function get_orders_id($id)
+	{
+
+		$id = $this->db->escape($id);
+
+		$sql ="SELECT m.* , u1.name create_by_name , u2.name  modified_by_name FROM  orders m
+						LEFT JOIN tbl_users u1 ON u1.userId = m.create_by
+						LEFT JOIN tbl_users u2 ON u2.userId = m.modified_by
+						WHERE m.orders_id = ".$id;
+		$query = $this->db->query($sql);
+		$row = $query->row_array();
+		return $row;
+	}
+
+	function save_orders($orders_info)
+	{
+			$this->db->trans_start();
+			$this->db->insert('orders', $orders_info);
+			$insert_id = $this->db->insert_id();
+			$this->db->trans_complete();
+			return $insert_id;
+	}
+
+	function update_orders($orders_info,$id)
+	{
+			$this->db->where('orders_id', $id);
+			$this->db->update('orders', $orders_info);
+			return TRUE;
+	}
+
+}
+
+/* End of file orders_model.php */
+/* Location: ./application/models/orders_model.php */
